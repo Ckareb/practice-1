911fa78fa55f28dd5f6cd4bd16c8eb7d40cdd6c3
Valeryi <valerabudkov20@gmail.com>
2022-09-23 21:17:38 +0300

    o

---
+++ pr3/task9.py
@@ -0,0 +1,10 @@
+def fin():
+    if (m*n) % k == 0:
+        print('Да')
+    else: print('Нет')
+
+n = int(input('Число долек один '))
+m = int(input('Число додек два '))
+k = int(input('Число долек деления '))
+
+fin()
