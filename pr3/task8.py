8989d6b6d1dca3dad181d795672b2658a663bb99
Valeryi <valerabudkov20@gmail.com>
2022-09-23 20:15:04 +0300

    initial commit

--- pr3/task8.py
+++ pr3/task8.py
@@ -1,6 +1,17 @@
+def fin():
+    if number1 == number2 == number3:
+        print('3')
+    elif number1 == number2:
+        print('2')
+    elif number2 == number3:
+        print('2')
+    elif number1 == number3:
+        print('2')
+    else: print('0')
+
+
 number1 = int(input('Число один '))
 number2 = int(input('Число два '))
 number3 = int(input('Число три '))
 
-
-
+fin()
