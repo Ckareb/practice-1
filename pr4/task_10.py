d5d9cc16b382a196a0839a195d576513f9d10b37
Valeryi <valerabudkov20@gmail.com>
2022-10-02 22:14:33 +0300

    commit

--- pr4/task_10.py
+++ pr4/task_10.py
@@ -2,10 +2,11 @@ def cycle():
     x = 1 
     global n, count_1, count_2, k
     for i in range(2,n):
-        i = count_1 + count_2
+        x = count_1 + count_2
         count_1 = count_2
-        count_2 = i 
-        print(count_2)
+        count_2 = x
+        if k <= i: 
+            print(count_2)
 
 
 n = int(input('Количество переменных '))
