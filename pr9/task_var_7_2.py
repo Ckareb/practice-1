bc82e38e59a62b70d6ebd15e9e3b0b1330d15d76
Valeryi <valerabudkov20@gmail.com>
2022-11-06 15:34:48 +0300

    commit

--- pr9/task_var_7_2.py
+++ pr9/task_var_7_2.py
@@ -1,39 +1,47 @@
+def inMat():
+    for i in range(N):
+        Y = []
+        for i in range(N):
+            Y.append(int(input('Внести значение в массив ')))
+        X.append(Y)
+    for i in range(N):
+        for j in range(N):
+            print(X[i][j], end=' ')
+        print()
+
+
+def Mas():
+    for i in range(N):
+        for j in range(N):
+            if (i == j) | (i + j == N - 1):
+                A.append(X[i][j])
+    print(A)
+
+def sumMas():
+    C = len(A)
+    D = 0
+    for i in range(C):
+        D += A[i]  
+    print(D)
+    return D
+   
+
+def Trans(D):
+    for i in range(N):
+        for j in range(N):
+            if i % 2 == 0:
+                X[i][j] = X[i][j] / D
+
+    for i in range(N):
+        for j in range(N):
+            print(X[i][j], end=' ')
+        print()
+
 N = int(input('Введите количество столбцов и строк квадратной матрицы '))
 X = []
-for i in range(N):
-    Y = []
-    for i in range(N):
-        Y.append(int(input('Внести значение в массив ')))
-    X.append(Y)
+inMat()
 
 A = []
+Mas()
 
-for i in range(N):
-    for j in range(N):
-        print(X[i][j], end=' ')
-    print()
-
-for i in range(N):
-    for j in range(N):
-        if (i == j) | (i + j == N - 1):
-            A.append(X[i][j])
-
-print(A)
-
-C = len(A)
-D = 0
-for i in range(C):
-    D += A[i] 
-
-print(D)
-
-for i in range(N):
-    for j in range(N):
-        if i % 2 == 0:
-            X[i][j] = X[i][j] / D
-
-for i in range(N):
-    for j in range(N):
-        print(X[i][j], end=' ')
-    print()
-            
+Trans(sumMas())            
